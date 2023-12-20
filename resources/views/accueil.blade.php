b526a8bee16e2a776f257fb23f96645e68061030
DEMORY Mael <mael_demory@ens.univ-artois.fr>
2023-12-20 15:38:43 +0100

    Ajout d'un bouton pour aller sur une vue qui affiche toutes les histoires

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,9 +1,13 @@
 <x-layout>
 
     <div class="container">
-        <h1>Bienvenue sur notre site</h1>
+        <h1 style="margin-top: 15px">Bienvenue sur notre site</h1>
+        <div class="text-center">
+            <a style="margin-bottom: 25px" href="{{ route('histoires.index') }}" class="btn btn-primary mx-auto">Voir toutes les histoires</a>
+        </div>
         <div class="row">
             @foreach($histoires as $histoire)
+
                 <div class="col-md-4 mb-4">
                     <div class="card">
                         <img src="{{ asset($histoire->photo) }}" class="card-img-top" alt="{{ $histoire->titre }}">
