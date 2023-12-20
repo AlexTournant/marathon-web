67f9e4065f7dfef5f007eb06d0cf30a64d2ff015
DEMORY Mael <mael_demory@ens.univ-artois.fr>
2023-12-20 10:19:51 +0100

    initial commit'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div style="display: flex;align-items: center; justify-content: center">
         <div>
+            Groupe 13
             <b>Le marathon du WEB 2023 !!!</b>
         </div>
     </div>
