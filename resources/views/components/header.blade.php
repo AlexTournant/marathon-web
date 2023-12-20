00748be59b636b6f49c5877a2148d620da0c1b0d
dylan <dylan_ballet@ens.univ-artois.fr>
2023-12-20 13:34:52 +0100

    ✨ Modification des composants et la page d'accueil

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <nav style="background-color: #1a202c" class="navbar navbar-expand-lg navbar-dark p-2">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Raytracing</a>
+        <a class="navbar-brand" href="/">HistoryMaker</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -15,24 +15,33 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact</a>
                 </li>
+                @guest
+                    <li class="m-2" id="banner"><a href="{{ route('login') }}">Login</a></li>
+                    <li class="m-2" id="banner"><a href="{{ route('register') }}">Register</a></li>
+                @endguest
                 @auth
-                    <li>
-                    {{Auth::user()->name}}
-                    <a href="{{route("logout")}}"
-                       onclick="document.getElementById('logout').submit(); return false;">Logout</a>
-                    <form id="logout" action="{{route("logout")}}" method="post">
-                        @csrf
-                    </form>
-                @else
-                    <li>
-                        <a href="{{route("login")}}">Login</a>
-                    </li>
-                    <a href="{{route("register")}}">Register</a>
-                    </li>
-                @endauth
+                    <li class="text-white m-2 fw-bold"> Bonjour {{ Auth::user()->name }}</li>
+                    @if (Auth::user())
 
+                        <li class="m-2 end-100" id="banner"><a href="#">Des liens spécifiques pour utilisateurs connectés...</a></li>
+                    @endif
+                    <li class="m-2" id="banner"><a href="#" id="logout">Logout</a></li>
+                    <form id="logout-form" action="{{ route('logout') }}"
+                          method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                    <div class="image-container ratio ratio-1x1 rounded-circle overflow-hidden" style="max-height: 5vh; max-width: 5vh;">
+                        <a href="/users/{{Auth::user()->id}}">            <img src="{{asset("/storage/avatars/".\App\Models\User::all()->find(Auth::id())->get("avatar")[Auth::id()-1]["avatar"])}}" class="img-fluid " alt="Votre Image " >
+                        </a>
+                    </div>
+
+                    <script>
+                        document.getElementById('logout').addEventListener("click", (event) => {
+                            document.getElementById('logout-form').submit();
+                        });
+                    </script>
+                @endauth
             </ul>
-            <x-banner class="ms-auto"></x-banner>
         </div>
     </div>
 </nav>
