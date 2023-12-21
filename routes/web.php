5756607f0f9578248690d4809834af4534a4c143
dylan <dylan_ballet@ens.univ-artois.fr>
2023-12-21 08:44:05 +0100

    ✨ Modification de la route pour afficher l'équipe

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\EquipeController;
 use App\Http\Controllers\HistoireController;
 use Illuminate\Support\Facades\Route;
@@ -18,9 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',  [HistoireController::class, 'accueil'])->name('accueil');
 
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name("contact");
+Route::get('/contact', [ContactController::class, 'afficherFormulaire']);
+
+Route::get('/formulaire', [ContactController::class, 'show']);
+Route::post('/contact', [ContactController::class, 'traiterFormulaire']);
+
 
 Route::get('/test-vite', function () {
     return view('test-vite');
@@ -36,4 +39,4 @@ Route::get('/chapitre/{id}',[\App\Http\Controllers\ChapitreController::class,'sh
 
 Route::resource('histoires', HistoireController::class);
 
-Route::get('/equipes/index', [EquipeController::class, 'index'])->name('equipes.index');
+Route::get('/apropos', [EquipeController::class, 'index'])->name('equipes.index');
