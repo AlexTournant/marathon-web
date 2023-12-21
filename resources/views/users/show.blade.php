88ad2898d0dc02d498688f1115c1a5e98d48ec6d
Alex Tournant <alex.tournant.1@gmail.com>
2023-12-21 15:37:52 +0100

    ajout des vues de l'utilisateur sur son profil

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,8 +17,42 @@
         @endauth
         <p class="text-center mb-1"><strong>Adresse de courriel</strong></p>
         <p class="text-center mb-4"><a href="mailto:{{$user['email']}}">{{ $user['email'] }}</a></p>
-        @if(count($user->mesHistoires))
-            <a href="{{route('histoires.index',['histoires'=>\App\Models\User::find(\Illuminate\Support\Facades\Auth::id())->mesHistoires()->get()])}}">Histoires crees</a>
-        @endif
+    </div>
+    <div class="row">
+        @foreach(App\Models\User::all()->find($user["id"])->mesHistoires()->get() as $histoire )
+            @if($histoire->active==1 or $histoire->user_id===Auth::id())
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="{{ asset($histoire->photo) }}" class="card-img-top" alt="{{ $histoire->titre }}">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="{{ route('histoires.show', $histoire->id) }}">{{ $histoire->titre }}</a>
+                            </h5>
+                            <p class="card-text">{{ $histoire->pitch }}</p>
+                            @auth()
+                                <p class="card-text"><small class="text-muted">{{ $histoire->active ? 'Actif' : 'Inactif' }}</small></p>
+                            @endauth
+                            <p class="card-text">
+                                Utilisateur: <a href="{{ route('users.show', $histoire->user->id) }}">{{ $histoire->user->id}}</a>
+                            </p>
+                            <p class="card-text">Genre: <a href="{{route('genre', $histoire->genre->id)}}"> {{  $histoire->genre->label }} </a></p>
+                            <p class="card-text">Genre: <a href="{{route('genre', $histoire->genre->id)}}"> En savoir plus </a></p>
+
+                            @auth
+                                @php
+                                    $userCompletedStory = Auth::user()->terminees->contains($histoire);
+                                @endphp
+
+                                @if ($userCompletedStory)
+                                    L'utilisateur a terminé cette histoire.
+                                @else
+                                    L'utilisateur n'a pas terminé cette histoire.
+                                @endif
+                            @endauth
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
     </div>
 </x-layout>
