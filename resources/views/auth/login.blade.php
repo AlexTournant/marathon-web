f389e4780f0b398a3d548ceaff5762c322e26ef6
DEMORY Mael <mael_demory@ens.univ-artois.fr>
2023-12-21 08:47:55 +0100

    Correction des formulaires de login et de register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,14 @@
 <div class="container">
     <div class="content">
 <h1>Connexion</h1>
-<input type="text" placeholder="Nom d'utilisateur">
-<input type="text" placeholder="Mot de passe">
+        <div class="form-group">
+            <label for="Nom d'utilisateur">Nom d'utilisateur</label>
+            <input type="text" name="nom d'utilisateur" class="form-input" placeholder="Nom d'utilisateur">
+        </div>
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" class="form-input" placeholder="Mot de passe">
+        </div>
 <div class='align'><input type="checkbox"><p>Se souvenir de moi</p></div>
 <input class="btn" type="submit">
 <p>Pas encore de compte ? <a href="{{route('register')}}">S'inscrire</a></p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,14 +1,36 @@
 <x-main>
 
-<div class="container">
-    <div class="content">
-<h1>Inscription</h1>
-<input type="text" placeholder="Nom d'utilisateur">
-<input type="text" placeholder="E-mail">
-<input type="text" placeholder="Mot de Passe">
-<input type="text" placeholder="Confirmer mot de passe">
-<input class="btn" type="submit">
-<p>Déjà inscrit ? <a href="{{route('login')}}">Se connecter</a></p>
-</div>
-</div>
+    <div class="container">
+        <div class="content">
+            <h1>Inscription</h1>
+
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div class="form-group">
+                    <label for="username">Nom d'utilisateur</label>
+                    <input type="text" name="username" class="form-input" placeholder="Nom d'utilisateur">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" class="form-input" placeholder="email@example.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" class="form-input" placeholder="Mot de passe">
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">Confirmer le mot de passe</label>
+                    <input type="password" name="password_confirmation" class="form-input" placeholder="Confirmer le mot de passe">
+                </div>
+
+                <button type="submit" class="btn btn-primary">S'inscrire</button>
+            </form>
+
+            <p>Déjà inscrit ? <a href="{{ route('login') }}">Se connecter</a></p>
+        </div>
+    </div>
 </x-main>
